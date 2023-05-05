edca1e7c60c58c0d97c9fb91757b3078f453c6b7
Jim Pick <jim@jimpick.com>
2023-05-04 19:58:33 -0700

    Switch regex back to sed from perl

--- patch-lotus.sh
+++ patch-lotus.sh
@@ -2,8 +2,12 @@
 
 cd lotus/build
 
-perl -pi \
-  -e 's/var UpgradeLightningHeight = abi.ChainEpoch\(30\)/var UpgradeLightningHeight = abi.ChainEpoch(-22)/;' \
-  -e 's/var UpgradeThunderHeight = abi.ChainEpoch\(1000\)/var UpgradeThunderHeight = abi.ChainEpoch(-23)/;' \
-  -e 's/const GenesisNetworkVersion = network.Version18/const GenesisNetworkVersion = network.Version20/;' \
-  params_2k.go
+sed 's/var UpgradeLightningHeight = abi.ChainEpoch(30)/var UpgradeLightningHeight = abi.ChainEpoch(-22)/' params_2k.go > params_2k.go2
+mv -f params_2k.go2 params_2k.go
+
+sed 's/var UpgradeThunderHeight = abi.ChainEpoch(1000)/var UpgradeThunderHeight = abi.ChainEpoch(-23)/' params_2k.go > params_2k.go2
+mv -f params_2k.go2 params_2k.go
+
+sed 's/const GenesisNetworkVersion = network.Version18/const GenesisNetworkVersion = network.Version20/' params_2k.go > params_2k.go2
+mv -f params_2k.go2 params_2k.go
+

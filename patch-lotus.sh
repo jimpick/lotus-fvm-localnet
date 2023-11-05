77dc3026ae08c44854f440860376163c877f98ec
Jim Pick <jim@jimpick.com>
2023-11-05 08:33:32 -0800

    Set network to watermelon

--- patch-lotus.sh
+++ patch-lotus.sh
@@ -2,12 +2,9 @@
 
 cd lotus/build
 
-sed 's/var UpgradeLightningHeight = abi.ChainEpoch(30)/var UpgradeLightningHeight = abi.ChainEpoch(-22)/' params_2k.go > params_2k.go2
+sed 's/var UpgradeWatermelonHeight = abi.ChainEpoch(200)/var UpgradeThunderHeight = abi.ChainEpoch(-24)/' params_2k.go > params_2k.go2
 mv -f params_2k.go2 params_2k.go
 
-sed 's/var UpgradeThunderHeight = abi.ChainEpoch(1000)/var UpgradeThunderHeight = abi.ChainEpoch(-23)/' params_2k.go > params_2k.go2
-mv -f params_2k.go2 params_2k.go
-
-sed 's/const GenesisNetworkVersion = network.Version18/const GenesisNetworkVersion = network.Version20/' params_2k.go > params_2k.go2
+sed 's/const GenesisNetworkVersion = network.Version20/const GenesisNetworkVersion = network.Version21/' params_2k.go > params_2k.go2
 mv -f params_2k.go2 params_2k.go
 

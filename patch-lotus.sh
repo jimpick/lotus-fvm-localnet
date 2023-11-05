6587f4f2a3adc85e11e5a60e5459f82d252cb240
Jim Pick <jim@jimpick.com>
2023-11-05 08:42:43 -0800

    Fix patch

--- patch-lotus.sh
+++ patch-lotus.sh
@@ -2,7 +2,7 @@
 
 cd lotus/build
 
-sed 's/var UpgradeWatermelonHeight = abi.ChainEpoch(200)/var UpgradeThunderHeight = abi.ChainEpoch(-24)/' params_2k.go > params_2k.go2
+sed 's/var UpgradeWatermelonHeight = abi.ChainEpoch(200)/var UpgradeWatermelonHeight = abi.ChainEpoch(-24)/' params_2k.go > params_2k.go2
 mv -f params_2k.go2 params_2k.go
 
 sed 's/const GenesisNetworkVersion = network.Version20/const GenesisNetworkVersion = network.Version21/' params_2k.go > params_2k.go2
